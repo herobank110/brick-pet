d5c9bc61022c5c01cd72c79850b9fc4649ca109a
David Kanekanian <dkanekanian1@gmail.com>
2021-04-01 20:53:56 +0100

    a sick line

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi');
+import { init } from './myEngine';
+
+function draw(ctx: CanvasRenderingContext2D) {
+  ctx.beginPath();
+  ctx.moveTo(1, 10);
+  ctx.lineTo(100, 10);
+  ctx.stroke();
+}
+
+init(draw);
---
+++ src/myEngine.ts
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+/** per frame callback for everything on a 2d canvas */
+type TickFn = (ctx: CanvasRenderingContext2D) => void;
+
+/** start the game engine */
+export function init(tickFn: TickFn) {
+  $(() => initImpl(tickFn));
+}
+
+function initImpl(tickFn: TickFn) {
+  const c = $('<canvas>') as JQuery<HTMLCanvasElement>;
+  $('body').append(c);
+
+  fillToWindow(c);
+  $(window).on('resize', () => fillToWindow(c));
+
+  function run() {
+    const ctx = c[0].getContext('2d');
+    if (!ctx) {
+      throw new Error("couldn't get context");
+    }
+    tickFn(ctx);
+    requestAnimationFrame(run);
+  }
+  requestAnimationFrame(run);
+}
+
+function fillToWindow(c: JQuery<HTMLCanvasElement>) {
+  const sz = { width: window.innerWidth, height: window.innerHeight };
+  c.attr(sz).css(sz);
+}
