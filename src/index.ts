a8f42d3c4273a16a5f4da532c690e2ccd89d7ed3
David <dkanekanian1@gmail.com>
2021-04-02 18:39:10 +0100

    add style

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import MyEngine from './myEngine';
+import { iota } from './utils';
+import $ from 'jquery';
 
 type State = {
   a: string;
 };
 
+const makePalette = (data: { n: number }) =>
+  $('<div>', { class: 'palette' }).append(
+    $('<h4>', { class: 'headline', text: 'Make a Selection' }),
+    $('<div>', { class: 'palette__grid' }).append(
+      iota(data.n).map(i => $('<div>', { class: 'palette__cell' })),
+    ),
+  );
+
 function init(): State {
+  $('#palette').append(makePalette({ n: 10 }));
+
   return {
     a: 'orange',
   };
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function iota(n: number) {
+  const ret = [];
+  for (let i = 0; i < n; i++) {
+    ret.push(i);
+  }
+  return ret;
+}
